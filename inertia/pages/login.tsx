f47122d2454963c95d3c23956d261baac46154c3
isoppp <prog@d2deck.com>
2024-09-14 23:16:37 +0900

    use inertia router to submit form

--- inertia/pages/login.tsx
+++ inertia/pages/login.tsx
@@ -1,22 +1,23 @@
-import { Head } from '@inertiajs/react'
-import { useEffect, useState } from 'react'
+import { Head, router } from '@inertiajs/react'
+import { useState } from 'react'
 import { Layout } from '~/components/Layout'
 
-
 export default function Home({}) {
-  const [csrf, setCSRF] = useState('')
-  useEffect(() => {
-    const el = document.querySelector<HTMLInputElement>('#csrf_for_inertia')
-    el && setCSRF(el.value)
-  }, [])
+  const [values, setValues] = useState({
+    email: 'test@example.com',
+    password: 'password',
+  })
 
+  function handleSubmit(e) {
+    e.preventDefault()
+    router.post('/sessions', values)
+  }
   return (
     <Layout>
       <Head title="Homepage" />
       login
       <div>
-        <form method="post" action="/sessions" className="grid gap-4">
-          <input type="hidden" name="_csrf" value={csrf} />
+        <form className="grid gap-4" onSubmit={handleSubmit}>
           <input type="email" name="email" placeholder="Email" defaultValue="test@example.com" />
           <input type="password" name="password" placeholder="Password" defaultValue="password" />
           <button type="submit">Login</button>
