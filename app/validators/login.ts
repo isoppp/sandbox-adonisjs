808607d242fee80e8c6814498af254892f28f9be
isoppp <prog@d2deck.com>
2024-09-14 23:38:07 +0900

    add vine and react-hook-form

---
+++ app/validators/login.ts
@@ -0,0 +1,9 @@
+import vine from '@vinejs/vine'
+import { Infer } from '@vinejs/vine/types'
+
+export const loginSchema = vine.object({
+  email: vine.string().email(),
+  password: vine.string().minLength(6),
+})
+export type LoginSchema = Infer<typeof loginSchema>
+export const loginValidator = vine.compile(loginSchema)
--- inertia/pages/login.tsx
+++ inertia/pages/login.tsx
@@ -1,25 +1,36 @@
+import { LoginSchema, loginValidator } from '#validators/login'
+import { vineResolver } from '@hookform/resolvers/vine'
 import { Head, router } from '@inertiajs/react'
-import { useState } from 'react'
+import { useCallback } from 'react'
+import { useForm } from 'react-hook-form'
 import { Layout } from '~/components/Layout'
 
 export default function Home({}) {
-  const [values, setValues] = useState({
-    email: 'test@example.com',
-    password: 'password',
+  const {
+    formState: { errors },
+    register,
+    handleSubmit,
+  } = useForm({
+    resolver: vineResolver(loginValidator),
+    defaultValues: {
+      email: 'test@example.com',
+      password: 'password',
+    },
   })
 
-  function handleSubmit(e) {
-    e.preventDefault()
-    router.post('/sessions', values)
-  }
+  const onSubmit = useCallback((data: LoginSchema) => {
+    router.post('/sessions', data)
+  }, [])
   return (
     <Layout>
       <Head title="Homepage" />
       login
       <div>
-        <form className="grid gap-4" onSubmit={handleSubmit}>
-          <input type="email" name="email" placeholder="Email" defaultValue="test@example.com" />
-          <input type="password" name="password" placeholder="Password" defaultValue="password" />
+        <form className="grid gap-4" onSubmit={handleSubmit(onSubmit)}>
+          <input type="email" placeholder="Email" {...register('email')} />
+          {errors.email && <div className="text-red-600">{errors.email.message}</div>}
+          <input type="password" placeholder="Password" {...register('password')} />
+          {errors.password && <div className="text-red-600">{errors.password.message}</div>}
           <button type="submit">Login</button>
         </form>
       </div>
